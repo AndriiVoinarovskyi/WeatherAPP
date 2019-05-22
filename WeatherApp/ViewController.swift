f9063f304929a37c32055ab4ec9841ebfdfd9a59
Andrii Voinarovskyi <d3adly.v3n0m@gmail.com>
2019-05-22 15:28:34 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Macbook Pro 13 on 05.22.19.
+//  Copyright © 2019 My Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
